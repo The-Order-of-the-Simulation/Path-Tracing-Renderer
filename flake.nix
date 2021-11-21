419c7ab54d23fd079e682e8b1a6fc386086f8716
Abbas G <abbas@abbasg.com>
2021-11-21 14:24:16 +0300

    Added a Nix Flake for easier compilation using Nix.

---
+++ flake.nix
@@ -0,0 +1,45 @@
+
+{
+  description = "A flake for building Path-Tracing_Renderer";
+  inputs = {
+    nixpkgs = {
+      url = "nixpkgs/nixos-21.05";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      path-tracing-renderer = (with pkgs; stdenv.mkDerivation {
+          pname = "path-tracing-renderer";
+          version = "1.0.0";
+          src = ./.;
+          nativeBuildInputs = [
+            gcc
+            glm
+          ];
+          buildPhase = "g++ -march=native -O2 -s -std=c++11 -Wall -Wextra -Wshadow \"./src/image.cpp\" \"./src/rng.cpp\" \"./src/main.cpp\" -o \"./OpenPT\"";
+          installPhase = ''
+            mkdir -p $out/bin
+            mv OpenPT $out/bin
+          '';
+        }
+      );
+    in rec {
+      defaultApp = flake-utils.lib.mkApp {
+        drv = defaultPackage;
+      };
+      defaultPackage = path-tracing-renderer;
+      devShell = pkgs.mkShell {
+        buildInputs = [
+          path-tracing-renderer
+        ];
+      };
+    }      
+  );
+}
